0b6084a6511a4989c7393e993aa6dc17f05127c7
Djemal0 <124554625+Djemal0@users.noreply.github.com>
2023-03-01 00:30:03 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿namespace _4асове
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        
+           Console.WriteLine("Vuvedete chislo ot 1 do 7");
+            int number = int.Parse(Console.ReadLine());
+            switch (number)
+            {
+                case 1:
+             Console.WriteLine("Ponedelnik imame po 6 chasa na u4ilishte");
+                    break;
+             case 2:
+             Console.WriteLine("Vtornik imame po 8 chasa na u4ilishte");
+             break;
+             case 3:
+             Console.WriteLine("Srqda imame po 7 chasa na u4ilishte");
+             break;
+             case 4: Console.WriteLine("4hetvurtuk  imame po 7 chasa na u4ilishte");
+             break;
+             case 5: Console.WriteLine("Patuk imame po 6 chasa na u4ilishte");
+             break;
+             case 6 & 7:
+             Console.WriteLine("Subota i Nedelq nqma u4ilishte");
+             break;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
